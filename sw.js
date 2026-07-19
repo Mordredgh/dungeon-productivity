8df29b00ffd2b1d1f54cbf37c5d028ab9ce76bcb
Mordredgh <gerardosilvar16@gmail.com>
2026-07-19 10:20:25 -0600

    fix: compact battle actions and pet card

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* ArcANUM service worker: núcleo pequeño, arte bajo demanda.
    Nunca precachear sprites/fondos completos: el navegador los guarda al verlos. */
-const CACHE = 'dungeon-v277';
+const CACHE = 'dungeon-v278';
 const CORE = [
   '/',
   '/index.html',
