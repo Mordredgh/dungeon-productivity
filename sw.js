f6610f9e3ae39965b0adf97a4cfff471610bd00f
Mordredgh <gerardosilvar16@gmail.com>
2026-07-19 22:39:33 -0600

    deploy: publicar controles beta y supabase

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* ArcANUM service worker: núcleo pequeño, arte bajo demanda.
    Nunca precachear sprites/fondos completos: el navegador los guarda al verlos. */
-const CACHE = 'dungeon-v303';
+const CACHE = 'dungeon-v304';
 const CORE = [
   '/',
   '/index.html',
